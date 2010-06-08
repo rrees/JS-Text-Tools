b73798cecbf2d54fd6285ab2782dc176297a87b6
Robert Rees <rrees@thoughtworkers-macbook-pro-7.local>
2010-06-08 15:39:12 +0100

    Cleaning the string to avoid confusion in the counting.

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -8,5 +8,18 @@ describe 'TextTools'
 	it 'should count commas as word seperators'
 		TextTools.wordCount("Flow my tears, the policeman said").should.eql 6
 	end
+	
+	it 'should count punctuation as word seperators'
+		TextTools.wordCount("This is it!he cried.I can take it no longer;really!?Really").should.eql 13
+	end
+	
+	it 'should not count punctuation at the end of the text as a word'
+		TextTools.wordCount("What is going on?").should.eql 4
+	end
+
+	it 'should not count punctuation at the start of the text as a word'
+		TextTools.wordCount(".  what now?").should.eql 2
+	end
+
   end
 end
